0085a07007ddc6f67922c90ed7383044c43d2c37
Daniel Laufer <lauferkdaniel@gmail.com>
2022-01-28 21:14:27 -0500

    Connected front-end to back-end

--- front-end/src/modules/LandingPage/index.js
+++ front-end/src/modules/LandingPage/index.js
@@ -12,18 +12,26 @@ import {
 } from '@chakra-ui/react';
 import axios from 'axios';
 import groupOnBooks from '../../assets/images/grouponbooks.jpeg';
+import { apiURL } from '../../utils/constants';
 
 function LandingPage() {
-  const [names, setNames] = useState([]);
+  const [users, setUsers] = useState([]);
 
+  // we won't be making API calls directly in components in the future,
+  // instead we will be doing so in the redux action creators
+  // but we'll just do this for now to demo the three tier architecture
   useEffect(() => {
     axios
-      .get('http://localhost:3001/')
+      .get(`${apiURL}/users`)
       .then(res => {
-        setNames([...names, 'daniel']);
+        setUsers([...users, ...res.data]);
       })
       .catch(err => console.error(err));
   }, []);
+
+  useEffect(() => {
+    console.log("Updated 'users' state: \n", users);
+  }, [users]);
 
   return (
     <ChakraProvider theme={theme}>
@@ -32,11 +40,16 @@ function LandingPage() {
         <Center>
           <img src={groupOnBooks} alt="cat" />
         </Center>
-        Users retrieved from the back-end (which are stored in a database)
+        <Heading as="h8" size="md">
+          Users retrieved from the StudyTogether MongoDB Database:
+        </Heading>
         <Center>
           <UnorderedList>
-            {names.map((name, i) => (
-              <ListItem key={i}>{name}</ListItem>
+            {users.map((user, i) => (
+              <ListItem key={i}>
+                First name: {user.firstName}, Last name: {user.lastName}, email:{' '}
+                {user.email}
+              </ListItem>
             ))}
           </UnorderedList>
         </Center>
