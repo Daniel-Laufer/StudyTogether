8074e38decec0d2283e1a6195e9fcf41617584c7
mohamed <mohamed.issa@mail.utoronto.ca>
2022-01-31 20:12:55 -0800

    Updated user schema

--- back-end/models/user.model.js
+++ back-end/models/user.model.js
@@ -3,13 +3,47 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  role: { type: String, required: true },
-  verified: { type: Boolean, required: true },
+  firstName: {
+    type: String,
+    required: [true, 'firstName not provided'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'lastName not provided'],
+  },
+  email: {
+    type: String,
+    unique: [true, 'email already exists in database.'],
+    lowercase: true,
+    trim: true,
+    required: [true, 'email not provided'],
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: '{VALUE} is not a valid email!',
+    },
+  },
+  role: {
+    type: String,
+    required: [true, 'Please specify user role'],
+    enum: ['Student', 'TA', 'Tutor'],
+  },
+  verified: {
+    type: Boolean,
+    required: [true, 'verification state is not provided!'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is not provided'],
+  },
+  created: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
 });
 
-const user = mongoose.model('user', userSchema);
+const user = mongoose.model('User', userSchema);
 
 module.exports = user;
