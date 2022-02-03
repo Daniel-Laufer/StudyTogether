ec6b68908ad33df5e8a95cd1e6e8e86cf95a1612
Daniel Laufer <lauferkdaniel@gmail.com>
2022-02-02 20:46:23 -0500

    [STUD-31] Login and registration pages are now functional and connected to the back-end.

--- front-end/src/actions/Auth.js
+++ front-end/src/actions/Auth.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-undef */
 import axios from 'axios';
 import { apiURL } from '../utils/constants';
 /* eslint-disable no-console */
@@ -7,35 +9,66 @@ export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const REGISTRATION_REQUEST = 'REGISTRATION_REQUEST';
 export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS';
 export const REGISTRATION_ERROR = 'REGISTRATION_ERROR';
+export const SAVE_AUTH_DETAILS = 'SAVE_AUTH_DETAILS';
 
 export function login(userDetails) {
   return dispatch => {
     dispatch({ type: LOGIN_REQUEST });
     axios
-      .post(`${apiURL}/login`, {
-        email: userDetails.password,
-        username: userDetails.username,
+      .post(`${apiURL}/users/login`, {
+        email: userDetails.email,
+        password: userDetails.password,
       })
-      .then(res => console.log(res))
+      .then(res => {
+        dispatch({ type: REGISTRATION_SUCCESS });
+        dispatch({ type: SAVE_AUTH_DETAILS, ...res.data });
+        if (userDetails.rememberUser)
+          window.localStorage.setItem('authToken', res.data.token);
+      })
       .catch(err => {
-        dispatch({ type: LOGIN_ERROR, error: err.toString() });
+        let errMessage = err.toString();
+        if (
+          err.response &&
+          err.response.data &&
+          err.response.data.errors &&
+          err.response.data.errors.length
+        )
+          errMessage = err.response.data.errors[0].msg;
+        dispatch({ type: LOGIN_ERROR, error: errMessage });
       });
   };
 }
 export function register(userDetails) {
-  console.log(userDetails);
   return dispatch => {
     dispatch({ type: LOGIN_REQUEST });
     axios
-      .post(`${apiURL}/register`, {
-        email: userDetails.password,
+      .post(`${apiURL}/users/register`, {
+        email: userDetails.email,
         password: userDetails.password,
         role: userDetails.role,
         nickName: userDetails.userName,
+        alias: userDetails.userName,
+        firstName: userDetails.firstName,
+        lastName: userDetails.lastName,
       })
-      .then(res => console.log(res))
+      .then(res => {
+        dispatch({ type: LOGIN_SUCCESS });
+        dispatch({ type: SAVE_AUTH_DETAILS, ...res.data });
+        if (userDetails.rememberUser)
+          window.localStorage.setItem('authToken', res.data.token);
+      })
       .catch(err => {
-        dispatch({ type: LOGIN_ERROR, error: err.toString() });
+        let errMessage = err.toString();
+        if (err.response && typeof err.response.data === 'string') {
+          errMessage = err.response.data;
+        } else if (
+          err.response &&
+          err.response.data &&
+          err.response.data.errors &&
+          err.response.data.errors.length
+        )
+          errMessage = err.response.data.errors[0].msg;
+        dispatch({ type: LOGIN_ERROR, error: errMessage });
       });
   };
 }
