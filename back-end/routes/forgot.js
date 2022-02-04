026675cb9ba7dabfdc0421b040961d575d9b1801
johnlewczuk <thejohnlewczuk@gmail.com>
2022-02-04 17:47:39 -0500

    all done, now needs to be looked over/frontend

--- back-end/routes/forgot.js
+++ back-end/routes/forgot.js
@@ -1,30 +1,111 @@
 var express = require("express");
-var nodemailer = require('nodemailer');
+var crypto = require("crypto");
+var bcrypt = require("bcrypt");
+var nodemailer = require("nodemailer");
+
 var router = express.Router();
+let user = require("../models/user.model");
+let Token = require("../models/token.model");
+const { body, validationResult } = require("express-validator");
+const clientURL = "http://localhost:3000";
 
+router.post("/forgot", body("email").notEmpty(), async function (req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+  const { email } = req.body;
+  let person = await user.findOne({ email: `${email}` });
+  if (!person) {
+    res.send("Email not in system");
+    return;
+  }
 
-router.post("/", 
-async (req, res) => {
-  const {email} = req.body;
+  //code taken from https://blog.logrocket.com/implementing-a-secure-password-reset-in-node-js/
+  await Token.findOneAndDelete({ userId: person._id });
+  var saltRounds = 8;
+  var salt = await bcrypt.genSalt(saltRounds);
+  let resetToken = crypto.randomBytes(32).toString("hex");
+  const hash = await bcrypt.hash(resetToken, salt);
+  console.log("compare" + bcrypt.compareSync(resetToken, hash));
+  console.log(resetToken);
+  await new Token({
+    email: email,
+    token: hash,
+    createdAt: Date.now(),
+  }).save();
+  const link = `${clientURL}/forgot/reset?token=${resetToken}&email=${email}`;
+  // end of code taken from
+
+  //email code taken from https://nodemailer.com/about/
   let transporter = nodemailer.createTransport({
-    port: 587,
-    host: "smtp.ethereal.email",
+    service: "gmail",
     auth: {
-      user: 'timmothy.fadel25@ethereal.email',
-      pass: '2Tedb1bHW38Rb22YzE'
-          },
-    secure: false,
+      user: "studtogtest@gmail.com",
+      pass: "studtogtest#2",
+    },
+    secure: true,
   });
   let msg = await transporter.sendMail({
-    from: 'timmothy.fadel25@ethereal.email',  // sender address
-    to: `${email}`,   // list of receivers
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!',
-    });
+    from: "studtogtest@gmail.com", // sender address
+    to: `${email}`, // list of receivers
+    subject: "Password Reset Request",
+    text: `Click this link to reset your password: ${link}`,
+  });
   await transporter.sendMail(msg);
-  
-  res.send("email sent")
-})
 
+  res.send("email sent\nperson: " + person + "\nlink: " + link);
+});
+
+//this router.post was taken from https://blog.logrocket.com/implementing-a-secure-password-reset-in-node-js/
+router.post(
+  "/resetpass",
+  body("email").notEmpty(),
+  body("password").notEmpty().isLength({ min: 6 }),
+  body("token").notEmpty(),
+
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const email = req.body.email;
+    console.log(req.body.password);
+    let passwordResetToken = await Token.findOne({ email: `${email}` });
+    if (!passwordResetToken || passwordResetToken.length == 0) {
+      res.send("Invalid or expired password reset token");
+      console.log("here");
+      return;
+    }
+
+    const isValid = bcrypt.compareSync(
+      req.body.token,
+      passwordResetToken.token
+    );
+    if (!isValid) {
+      res.send("Invalid or expired password reset token");
+      return;
+    }
+
+    var saltRounds = 8;
+
+    user.updateOne(
+      { email: `${email}` },
+      { password: bcrypt.hashSync(req.body.password, saltRounds) },
+      function (err, docs) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Updated docs: ", docs);
+        }
+      }
+    );
+
+    await passwordResetToken.deleteOne();
+    res.send("Password updated");
+  }
+);
 
 module.exports = router;
