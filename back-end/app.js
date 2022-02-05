344ea7315df4a87c5aceb78e3b6e5a3ff086d0d1
johnlewczuk <thejohnlewczuk@gmail.com>
2022-02-04 21:25:54 -0500

    final commit, just needs testing

--- back-end/app.js
+++ back-end/app.js
@@ -1,50 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var cors = require('cors');
-require('dotenv').config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var cors = require("cors");
+require("dotenv").config();
 
-var usersRouter = require('./routes/users');
-var forgotRouter = require('./routes/forgot');
+var usersRouter = require("./routes/users");
+var forgotRouter = require("./routes/forgot");
 
 var app = express();
 
-
 // MiddleWare
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/users', usersRouter);
-app.use('/forgot', forgotRouter)
-
+app.use("/users", usersRouter);
+app.use("/forgot", forgotRouter);
 
 // Connect to MongoDB Atlas cluster
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
 
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
-
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
 });
