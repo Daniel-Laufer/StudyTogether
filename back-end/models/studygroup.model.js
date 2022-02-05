7ae4249d06d4d2d767e604103b019006ec671849
Maor Gornic <gornik.maor@gmail.com>
2022-02-04 19:41:56 -0500

    [STUD-27] added a route and endpoint for fetching individual study groups

--- back-end/models/studygroup.model.js
+++ back-end/models/studygroup.model.js
@@ -1,18 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const studygroupSchema = new Schema({
-  title: { type: String, required: true },
-  day: { type: Date, required: true },
-  contactInfo: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  availability: { type: String, required: true },
-  hostName: { type: String, required: true },
-  description: { type: String, required: true },
-  tags: { type: [String], required: true },
-}, { collection: 'studygroups' });
+const studygroupSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    time: { type: Date, required: true },
+    phone: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    curAttendees: { type: Number, min: 0, required: true },
+    maxAttendees: { type: Number, min: 2, required: true },
+    hostFirstName: { type: String, required: true },
+    hostLastName: { type: String, required: true },
+    description: { type: String, required: true },
+    tags: { type: [String], required: true },
+  },
+  { collection: 'studygroups' }
+);
 
-const studygroup = mongoose.model("studygroup", studygroupSchema);
+const studygroup = mongoose.model('studygroup', studygroupSchema);
 
 module.exports = studygroup;
