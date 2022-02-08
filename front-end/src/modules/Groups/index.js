5f5249aa992d5d94d1eff952fdead8e6d8a2dd7b
Daniel Laufer <lauferkdaniel@gmail.com>
2022-02-08 13:31:56 -0500

    [STUD-33] fixed import.

--- front-end/src/modules/Groups/index.js
+++ front-end/src/modules/Groups/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import Group from '../../components/group';
+import Group from '../../components/Group';
 import Cat from '../../assets/images/cat.jpeg';
 import SecondGroup from '../../components/SecondGroup';
 
