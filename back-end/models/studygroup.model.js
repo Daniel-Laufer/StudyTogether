a13009707d411779428ae1b26a2bab431deff4a5
Maor Gornic <47336232+MaorGornic@users.noreply.github.com>
2022-02-09 11:21:58 -0500

    [STUD-37] Studygroup CRUD backend operations (#11)
    * [STUD-37] Created endpoints for creating, updating and deleting study groups
    
    * [STUD-37] Added authentication handling and error checking to endpoints
    
    * [STUD-37] Added study group routes documentation
    
    * [STUD-37] Added start time, end time and location fields to study group schema
    
    * [STUD-37] Added tests for editing, removing and creating study groups

--- back-end/models/studygroup.model.js
+++ back-end/models/studygroup.model.js
@@ -5,13 +5,14 @@ const Schema = mongoose.Schema;
 const studygroupSchema = new Schema(
   {
     title: { type: String, required: true },
-    time: { type: Date, required: true },
+    startDateTime: { type: Date, required: true },
+    endDateTime: { type: Date, required: true },
     phone: { type: String, required: true },
     imageUrl: { type: String, required: true },
-    curAttendees: { type: Number, min: 0, required: true },
+    curAttendees: { type: Number, min: 0, default: 0, required: true },
+    location: { type: { long: Number, lat: Number }, required: true },
     maxAttendees: { type: Number, min: 2, required: true },
-    hostFirstName: { type: String, required: true },
-    hostLastName: { type: String, required: true },
+    hostId: { type: mongoose.Types.ObjectId, required: true },
     description: { type: String, required: true },
     tags: { type: [String], required: true },
   },
--- back-end/routes/studygroups.js
+++ back-end/routes/studygroups.js
@@ -1,19 +1,122 @@
 var express = require('express');
 var router = express.Router();
 let StudygroupModel = require('../models/studygroup.model');
+var helperUser = require('../helpers/helperUser');
+const { body, validationResult } = require('express-validator');
 
 /* get all study groups*/
-router.get('/', function (req, res) {
+router.get('/', helperUser.verifyToken, (req, res) => {
+  // checking if user is authenticated
+  if (!req.user) {
+    res.status(401).send({ message: 'Invalid JWT token' });
+    return;
+  }
   StudygroupModel.find()
     .then(studygroups => res.status(200).json(studygroups))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 /* get an individual study group by ID*/
-router.get('/:id', function (req, res) {
+router.get('/:id', helperUser.verifyToken, (req, res) => {
+  // checking if user is authenticated
+  if (!req.user) {
+    res.status(401).send({ message: 'Invalid JWT token' });
+    return;
+  }
   const groupId = req.params.id;
   StudygroupModel.findById(groupId)
     .then(studygroup => res.status(200).json(studygroup))
+    .catch(err => res.status(400).json('Error: Invalid study group id'));
+});
+
+/* catching a post request with url ./create */
+router.post(
+  '/create',
+  helperUser.verifyToken,
+  /* Parameter Validation */
+  body('title').notEmpty(),
+  body('startDateTime').notEmpty(),
+  body('endDateTime').notEmpty(),
+  body('phone').notEmpty(),
+  body('imageUrl').notEmpty(),
+  body('location').exists().bail().isObject().bail().notEmpty(),
+  body('maxAttendees').notEmpty(),
+  body('tags').exists().bail().isArray().bail().notEmpty(),
+  (req, res) => {
+    // checking if user is authenticated
+    if (!req.user) {
+      res.status(401).send({ message: 'Invalid JWT token' });
+      return;
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    /* study group creation logic  */
+    let studygroup = new StudygroupModel({
+      title: req.body.title,
+      startDateTime: req.body.startDateTime,
+      endDateTime: req.body.endDateTime,
+      phone: req.body.phone,
+      imageUrl: req.body.imageUrl,
+      curAttendees: req.body.curAttendees,
+      location: req.body.location,
+      maxAttendees: req.body.maxAttendees,
+      hostId: req.user.id,
+      description: req.body.description,
+      tags: req.body.tags,
+    });
+
+    studygroup
+      .save()
+      .then(() => res.status(200).json('Study group created successfully!'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  }
+);
+
+/* editing a study group by id */
+router.patch('/edit/:id', helperUser.verifyToken, (req, res) => {
+  // check whether the user is authenticated as the host of this study group
+  if (!req.user) {
+    res.status(401).send({ message: 'Invalid JWT token' });
+    return;
+  }
+
+  const groupId = req.params.id;
+  StudygroupModel.findById(groupId)
+    .then(studygroup => {
+      if (studygroup.hostId != req.user.id) {
+        res.status(403).send({ message: 'Not study group creator' });
+        return;
+      }
+      Object.assign(studygroup, req.body);
+      studygroup.save();
+      res.status(200).json('Study group edited successfully!');
+    })
+    .catch(err => res.status(400).json('Error: Invalid study group id'));
+});
+
+/* deleting a study group by id */
+router.delete('/delete/:id', helperUser.verifyToken, (req, res) => {
+  // check whether the user is authenticated as the host of this study group
+  if (!req.user) {
+    res.status(401).send({ message: 'Invalid JWT token' });
+    return;
+  }
+
+  const groupId = req.params.id;
+  StudygroupModel.findById(groupId)
+    .then(studygroup => {
+      if (studygroup.hostId != req.user.id) {
+        res.status(403).send({ message: 'Not study group creator' });
+        return;
+      }
+      studygroup.delete();
+      res.status(200).json('Study group deleted successfully!');
+    })
     .catch(err => res.status(400).json('Error: Invalid study group id'));
 });
 
