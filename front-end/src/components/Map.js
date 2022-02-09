4c0b92f3a24a6d749a8050fb25720bf88a6487cc
Daniel Laufer <lauferkdaniel@gmail.com>
2022-02-09 11:36:39 -0500

    [STUD-15] Allow users to specify the study group location using the google map interactive component.

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -5,8 +5,10 @@
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable react/jsx-no-useless-fragment */
 import React, { useEffect } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import PropTypes from 'prop-types';
+
+// a lot of this code was taken from https://www.npmjs.com/package/@react-google-maps/api (the starter code provided in the documentation)
 
 const containerStyle = {
   height: '400px',
@@ -19,16 +21,28 @@ const defaultMapStyles = {
 };
 
 const center = {
-  lat: 43,
-  lng: -79,
+  lat: 43.54904,
+  lng: -79.662116,
 };
-function Map({ initialCenter, zoom, style }) {
+function Map({
+  initialCenter,
+  zoom,
+  style,
+  restrictToOneMarker,
+  getLngLatOfNewMarker,
+}) {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: 'AIzaSyD0UIR7Nc-oEEQ2ur9lY27J4Ewo4t1w0J0',
   });
 
   const [map, setMap] = React.useState(null);
+  const [markers, setMarkers] = React.useState([
+    {
+      lat: 43.54904,
+      lng: -79.662116,
+    },
+  ]);
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
@@ -40,21 +54,37 @@ function Map({ initialCenter, zoom, style }) {
     setMap(null);
   }, []);
 
-  useEffect(() => console.log(initialCenter, center), []);
+  const addMarker = ev => {
+    if (restrictToOneMarker) {
+      setMarkers([{ lat: ev.latLng.lat(), lng: ev.latLng.lng() }]);
+    } else
+      setMarkers([...markers, { lat: ev.latLng.lat(), lng: ev.latLng.lng() }]);
+
+    getLngLatOfNewMarker(ev.latLng.lat(), ev.latLng.lng());
+  };
 
   return isLoaded ? (
     <GoogleMap
-      center={center}
+      center={{
+        lat: 43.54,
+        lng: -79.66,
+      }}
       mapContainerStyle={containerStyle}
-      // center={center}
       onLoad={onLoad}
       onUnmount={onUnmount}
-      onClick={ev => console.log(ev)}
-      zoom={10}
+      onClick={addMarker}
+      zoom={16}
       style={{ ...defaultMapStyles, ...style }}
     >
-      Heyo
-      <></>
+      {markers.map(marker => (
+        <Marker
+          key={`${marker.lat}-${marker.lng}`}
+          position={{ lat: marker.lat, lng: marker.lng }}
+          onClick={() => {
+            setSelected(marker);
+          }}
+        />
+      ))}
     </GoogleMap>
   ) : (
     <></>
@@ -67,13 +97,17 @@ Map.propTypes = {
     lng: PropTypes.number,
   }),
   zoom: PropTypes.number,
+  restrictToOneMarker: PropTypes.bool,
+  getLngLatOfNewMarker: PropTypes.func,
 };
 Map.defaultProps = {
   initialCenter: {
     lat: 43.54,
     lng: -79.66,
   },
-  zoom: 17,
+  zoom: 20,
+  restrictToOneMarker: false,
+  getLngLatOfNewMarker: () => {},
 };
 
 export default React.memo(Map);
