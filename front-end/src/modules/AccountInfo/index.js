ae0457c577428ec7b2be09b84585ebabc502322e
milindvishnoi <milindvishnoi@gmail.com>
2022-02-24 19:07:03 -0500

    [STUD-51] Completed UI with edit feature for userInfo

--- front-end/src/modules/AccountInfo/index.js
+++ front-end/src/modules/AccountInfo/index.js
@@ -4,13 +4,18 @@ import {
   Image,
   Text,
   Input,
-  Button,
-  Box,
   Grid,
   GridItem,
+  Box,
+  Select,
+  Textarea,
+  ListItem,
+  UnorderedList,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { WithContext as ReactTags } from 'react-tag-input';
 import Cat from '../../assets/images/cat.jpeg';
+import GreenButton from '../../components/GreenButton';
 import SecondGroup from '../../components/SecondGroup';
 import * as colors from '../../utils/colors';
 
@@ -52,13 +57,30 @@ function AccountInfo() {
     name: 'Geralt Stanislav',
     desc: "I am a first year student at UTM who's majoring in CS, and trying to find like-minded people",
     role: 'Student',
-    snap: 'geralt.of.rivia',
-    ig: 'geralt.stan',
-    email: 'geralt.s@mail.utoronto.ca',
+    imgSrc: Cat,
+    contactInfo:
+      'You can reach me on Instagram, Snapchat, or email. I usually respond on the same day. IG: geralt.stan, Email: geralt.s@mail.utoronto.ca',
     interests:
       'I like playing the Witcher 3 on my PS4 whever I catch a break from my school-work, and watching Naruto. I also like working on side-projects pertaining to AI.',
-    courses: ['CSC108', 'MAT102', 'PHL247', 'MAT135'],
+    courses: [
+      { id: 'asdfasdfasdf', text: 'CSC301' },
+      { id: 'asdfasdfasdf', text: 'CSC302' },
+    ],
   });
+
+  const handleDelete = i => {
+    setUserInfo({
+      ...userInfo,
+      courses: [...userInfo.courses.filter((tag, index) => index !== i)],
+    });
+  };
+
+  const handleAddition = c => {
+    setUserInfo({
+      ...userInfo,
+      courses: [...userInfo.courses, c],
+    });
+  };
 
   const [edit, setEdit] = useState(false);
 
@@ -78,38 +100,119 @@ function AccountInfo() {
           w={['100%', '100%', '300px']}
         >
           <VStack w="full" alignItems="flex-start" justifyItems="left">
-            <Image
-              src={Cat}
-              borderRadius="full"
-              boxSize="200px"
-              alignSelf="center"
-            />
             {edit ? (
-              <VStack>
-                <Input
+              <VStack w="full" justifyContent="center">
+                <Box style={{ display: 'flex', alignItems: 'center' }} w="full">
+                  <Text justifySelf="flex-start" as="b" mr={1}>
+                    Image URL:
+                  </Text>
+                  <Input
+                    onChange={e =>
+                      setUserInfo({
+                        ...userInfo,
+                        imgSrc: e.target.value,
+                      })
+                    }
+                    value={userInfo.imgSrc}
+                    placeholder="Image URL"
+                  />
+                </Box>
+                <Box style={{ display: 'flex', alignItems: 'center' }} w="full">
+                  <Text justifySelf="flex-start" as="b" mr={1}>
+                    Name:
+                  </Text>
+                  <Input
+                    maxW="200px"
+                    onChange={e =>
+                      setUserInfo({
+                        ...userInfo,
+                        name: e.target.value,
+                      })
+                    }
+                    value={userInfo.name}
+                    placeholder="Name"
+                  />
+                </Box>
+                <Box style={{ display: 'flex', alignItems: 'center' }} w="full">
+                  <Text justifySelf="flex-start" as="b" mr={1}>
+                    Role:
+                  </Text>
+                  <Select
+                    width="175px"
+                    maxW="200px"
+                    defaultValue={userInfo.role}
+                  >
+                    <option value="Student">Student</option>
+                    <option value="TA">TA</option>
+                    <option value="Teacher">Teacher</option>
+                  </Select>
+                </Box>
+                <Text alignSelf="flex-start" as="b" mr={1}>
+                  Description:
+                </Text>
+                <Textarea
+                  value={userInfo.desc}
                   onChange={e =>
                     setUserInfo({
                       ...userInfo,
-                      name: e.target.value,
+                      desc: e.target.value,
                     })
                   }
-                  value={userInfo.name}
-                  placeholder="Name"
+                  size="md"
                 />
-                <Box>
-                  <Button
-                    width="200px"
-                    fontSize="20px"
-                    backgroundColor={colors.grey.dark}
-                    color={colors.white}
-                    onClick={() => setEdit(false)}
-                  >
-                    Save
-                  </Button>
-                </Box>
+                <Text alignSelf="flex-start" as="b" mr={1}>
+                  Contact Info:
+                </Text>
+                <Textarea
+                  value={userInfo.contactInfo}
+                  onChange={e =>
+                    setUserInfo({
+                      ...userInfo,
+                      contactInfo: e.target.value,
+                    })
+                  }
+                  size="md"
+                />
+                <Text alignSelf="flex-start" as="b" mr={1}>
+                  Interests:
+                </Text>
+                <Textarea
+                  value={userInfo.interests}
+                  onChange={e =>
+                    setUserInfo({
+                      ...userInfo,
+                      interests: e.target.value,
+                    })
+                  }
+                  size="md"
+                />
+                <Text alignSelf="flex-start" as="b" mr={1}>
+                  Courses I&apos;m taking:
+                </Text>
+                <ReactTags
+                  tags={userInfo.courses}
+                  // delimiters={delimiters}
+                  handleDelete={handleDelete}
+                  handleAddition={handleAddition}
+                  inputFieldPosition="bottom"
+                  autocomplete
+                />
+                <GreenButton
+                  width="200px"
+                  style={{ fontSize: '20px' }}
+                  onClick={() => setEdit(false)}
+                >
+                  Save
+                </GreenButton>
               </VStack>
             ) : (
               <VStack>
+                <Image
+                  src={userInfo.imgSrc}
+                  borderRadius="full"
+                  boxSize="200px"
+                  alignSelf="center"
+                />
                 <Text
                   fontSize={18}
                   as="b"
@@ -119,15 +222,48 @@ function AccountInfo() {
                   {userInfo.name} ({userInfo.role})
                 </Text>
                 <Text color={colors.grey.dark}>{userInfo.desc}</Text>
-                <Button
+                <GreenButton
                   width="200px"
-                  fontSize="20px"
-                  backgroundColor={colors.grey.dark}
-                  color={colors.white}
+                  style={{ fontSize: '20px' }}
                   onClick={() => setEdit(true)}
                 >
                   Edit
-                </Button>
+                </GreenButton>
+                <Text
+                  color={colors.grey.dark}
+                  as="b"
+                  alignSelf="flex-start"
+                  fontSize={18}
+                >
+                  Contact Info
+                </Text>
+                <Text color={colors.grey.dark}>{userInfo.contactInfo}</Text>
+                <Text
+                  color={colors.grey.dark}
+                  as="b"
+                  alignSelf="flex-start"
+                  fontSize={18}
+                >
+                  Interests
+                </Text>
+                <Text color={colors.grey.dark}>{userInfo.interests}</Text>
+                <Text
+                  color={colors.grey.dark}
+                  as="b"
+                  alignSelf="flex-start"
+                  fontSize={18}
+                >
+                  Courses I&apos;m taking
+                </Text>
+                <Box alignContent="flex-start" w="full">
+                  <Text>I&apos;m enrolled in:</Text>
+                  <UnorderedList>
+                    {userInfo.courses.length !== 0 &&
+                      userInfo.courses.map(c => (
+                        <ListItem ml={2}>{c.text}</ListItem>
+                      ))}
+                  </UnorderedList>
+                </Box>
               </VStack>
             )}
           </VStack>
