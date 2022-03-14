97c1f096f3d0407bdd5c69e75f15dcfca1372c0f
Maor Gornic <47336232+MaorGornic@users.noreply.github.com>
2022-03-13 21:55:55 -0400

    Revert "Stud 59/update studygroup view page" (#48)

--- front-end/src/components/DetailedGroup.js
+++ front-end/src/components/DetailedGroup.js
@@ -9,6 +9,7 @@ const diffSize = size =>
   size === 'lg'
     ? {
         stackSize: '100%',
+        imgWidth: ['300px', '350px', '400px'],
         vstackSpacing: '1.5rem',
         fontSize: '20px',
         noOfLines: '',
@@ -17,6 +18,7 @@ const diffSize = size =>
       }
     : {
         stackSize: { base: '300px', md: '400px' },
+        imgWidth: '150px',
         vstackSpacing: '6px',
         fontSize: '12px',
         noOfLines: '1',
@@ -24,19 +26,16 @@ const diffSize = size =>
         imgAlign: ['center', 'center', 'left'],
       };
 
-function CustomText(fontSize, noOfLines, titles, text) {
+function CustomText(fontSize, noOfLines, text) {
   return (
     <Text
-      fontWeight="bold"
+      as="b"
       color={colors.grey.dark}
       fontSize={fontSize}
       mt="0px"
       noOfLines={noOfLines}
     >
-      {`${titles} `}
-      <Text as="span" fontWeight="normal">
-        {text}
-      </Text>
+      {text}
     </Text>
   );
 }
@@ -46,8 +45,6 @@ function DetailedGroup({
   restrict,
   availability,
   when,
-  durationHours,
-  durationMins,
   host,
   desc,
   img,
@@ -58,8 +55,15 @@ function DetailedGroup({
   selected,
   status,
 }) {
-  const { stackSize, vstackSpacing, fontSize, noOfLines, imgMr, imgAlign } =
-    diffSize(size);
+  const {
+    stackSize,
+    imgWidth,
+    vstackSpacing,
+    fontSize,
+    noOfLines,
+    imgMr,
+    imgAlign,
+  } = diffSize(size);
 
   const groupView = (
     <Stack
@@ -71,7 +75,7 @@ function DetailedGroup({
       <VStack>
         <Image
           src={img}
-          width="400px"
+          width={imgWidth}
           alt={imgAlt}
           borderRadius="lg"
           mr={imgMr}
@@ -97,18 +101,12 @@ function DetailedGroup({
       </VStack>
 
       <VStack justifyContent="center" spacing={vstackSpacing} align="left">
-        {title && CustomText(fontSize, noOfLines, `Title: ${title}`)}
-        {CustomText(fontSize, noOfLines, 'Restriction:', `${restrict}`)}
-        {CustomText(fontSize, noOfLines, 'Availability:', `${availability}`)}
-        {CustomText(fontSize, noOfLines, 'When:', `${when}`)}
-        {CustomText(
-          fontSize,
-          noOfLines,
-          'Duration:',
-          `${durationHours} hour(s) ${durationMins} min(s)`
-        )}
-        {CustomText(fontSize, noOfLines, 'Hosted by:', `${host}`)}
-        {CustomText(fontSize, noOfLines, 'Description:', `${desc}`)}
+        {CustomText(fontSize, noOfLines, `Title: ${title}`)}
+        {CustomText(fontSize, noOfLines, `Restriction: ${restrict}`)}
+        {CustomText(fontSize, noOfLines, `Availability: ${availability}`)}
+        {CustomText(fontSize, noOfLines, `When: ${when}`)}
+        {CustomText(fontSize, noOfLines, `Hosted by: ${host}`)}
+        {CustomText(fontSize, noOfLines, `Description: ${desc}`)}
       </VStack>
     </Stack>
   );
@@ -157,8 +155,6 @@ DetailedGroup.propTypes = {
   availability: PropTypes.string.isRequired,
   host: PropTypes.string.isRequired,
   when: PropTypes.string.isRequired,
-  durationHours: PropTypes.string.isRequired,
-  durationMins: PropTypes.string.isRequired,
   desc: PropTypes.string.isRequired,
   size: 'md' || 'lg',
   onClickFunc: PropTypes.func,
